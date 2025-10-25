730b42418a827ee6d080f182c2b0d430d605d35c
Stadnik132 <diman.stadnikov@gmail.com>
2025-10-25 18:24:17 +0300

    Начинаем с нуля

--- Scenes/Units/Player/PlayerUnit.gd
+++
@@ -1,4 +0,0 @@
-extends "res://Scripts/Battle/BattleUnit.gd"
-
-func _ready():
-	setup_unit("Алексей", 100, 15, 5)
--- Scripts/Unit/PlayerBattle.gd
+++
@@ -1,7 +0,0 @@
-# PlayerBattle.gd  
-extends BattleUnitVisual  # ← НАСЛЕДУЕМ!
-
-func _ready():
-	var player_data = RelationshipManager.player_data
-	# setup_visual уже доступен через наследование
-	setup_visual(player_data, $Sprite2D)
